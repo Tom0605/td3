89f2134b7fec889b9978e5cd94c7b0802778b0cd
Tom <tom.vesleau1@gmail.com>
2024-02-19 16:41:05 +0100

    commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/ligne.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <menu>
+        <li>menu1</li>
+        <li>menu2</li>
+        <li>menu3</li>
+    </menu>
+</div>
+<h1> Hello world</h1>
+modifié
+</body>
+<div class="copyright"> copyright </div>
+</html>
